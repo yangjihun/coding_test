5a7b8fc268fa2e14cc406ba7eb87775ab5daf354
Jihun <yjhn0410@gmail.com>
2024-01-11 18:26:43 +0900

    1373

--- 11866.py
+++ 11866.py
@@ -1,10 +1,10 @@
 N,K = map(int,input().split())
-li = list(range(1,N+1))
+li = list(range(1,N+1))  # 0~N 까지의 수를 저장
 ans = []
-idx = 0
-while li:
-  idx = (idx+K-1)%len(li)
-  ans.append(li[idx])
-  li.pop(idx%len(li))
+idx = 0  # 삭제할 index값
+while li:  # list가 비워질 때까지 반복
+  idx = (idx+K-1)%len(li)  # li 범위를 벗어나지 않게 설정
+  ans.append(li[idx])  # answer list에 값 저장
+  li.pop(idx%len(li))  # 해당 값 삭제
 
 print('<'+str(ans)[1:-1]+'>')
---
+++ 1373.py
@@ -0,0 +1,15 @@
+jinsu_2 = input()
+jinsu_8 = ''
+
+jinsu_2 = '0'*(3-len(jinsu_2)%3) + jinsu_2
+
+for i in range(0,len(jinsu_2),3):
+  key = 2
+  add = 0
+  for k in jinsu_2[i:i+3]:
+    add += int(k)*(2**key)
+    key-=1
+  jinsu_8+=str(add)
+if jinsu_8[0]=='0' and len(jinsu_8)!=1:
+  jinsu_8 = jinsu_8[1:]
+print(jinsu_8)
