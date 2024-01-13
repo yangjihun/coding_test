bc9579a51dc390f9d4bc443c01b5b553de8b7ea2
Jihun <yjhn0410@gmail.com>
2024-01-14 00:10:25 +0900

    28278

--- 28278.py
+++ 28278.py
@@ -1,25 +1,18 @@
-li = list()
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
+li = []
 
 for i in range(n):
-  an = input()
+  an = sys.stdin.readline().split()
   if an[0]=="1":
-    li.append(int(an[1:]))
-  elif an=="2":
-    if len(li)==0:
-      print(-1)
-    else:
-      print(li[-1])
-      li.pop()
-  elif an=="3":
+    li.append(an[1])
+  elif an[0]=="2":
+    print(li.pop()) if len(li)!=0 else print(-1)
+  elif an[0]=="3":
     print(len(li))
-  elif an=="4":
-    if len(li)==0:
-      print(1)
-    else:
-      print(0)
-  elif an=="5":
-    if len(li)==0:
-      print(-1)
-    else:
-      print(li[-1])
+  elif an[0]=="4":
+    print(0) if len(li)!=0 else print(1)
+  elif an[0]=="5":
+    print(li[-1]) if len(li)!=0 else print(-1)
+
+# sys 추가하니까 타임에러 안뜸
