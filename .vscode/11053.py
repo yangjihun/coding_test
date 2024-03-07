1b78c18d851daea1650cc17ed6e12b901d8ca8d9
Jihun <yjhn0410@gmail.com>
2024-03-07 12:00:32 +0900

    move file to main directory

--- .vscode/11053.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-li = list(map(int,input().split()))
-A = [1]*N
-for i in range(1,N):
-  for j in range(i):
-    if li[i]>li[j]:
-      A[i] = max(A[i],A[j]+1)
-print(max(A))
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    print(deq[0] if deq else -1)
