35bab95ce41805979efdc3bec020461f603b1329
Jihun <yjhn0410@gmail.com>
2024-03-13 21:10:35 +0900

    update

--- 17404.py
+++ 17404.py
@@ -1,16 +1,19 @@
-# 1번 집의 색은 2번, N번 집의 색과 같지 않아야 한다.
-# N번 집의 색은 N-1번, 1번 집의 색과 같지 않아야 한다.
-# i(2 ≤ i ≤ N-1)번 집의 색은 i-1, i+1번 집의 색과 같지 않아야 한다.
-
 N = int(input())
-result = [[0,0],[0,0],[0,0]]
-first = list(map(int,input().split()))
-
-for i in range(2,N+1):
+r = [0,0,0]
+g = [0,0,0]
+b = [0,0,0]
+for i in range(N):
   rgb = list(map(int,input().split()))
-  if i==2:
-    result = [[rgb[0]+first[1],rgb[0]+first[2]],[rgb[1]+first[0],rgb[1]+first[2]],[rgb[2]+first[0],rgb[2]+first[1]]]
-  if i==N:
-    print(min([min([rgb[0]+min(result[1]),rgb[0]+min(first[2])]),min([rgb[1]+min(first[0]),rgb[1]+min(first[2])]),min([rgb[2]+min(first[0]),rgb[2]+min(first[1])])]))
+  if i==1:
+    r = [rgb[0]+min(r[1],r[2]),rgb[1]+r[0],rgb[2]+r[0]]
+    g = [rgb[0]+g[0],rgb[1]+min(g[0],g[2]),rgb[2]+g[0]]
+    g = [rgb[0]+b[2],rgb[1]+b[2],rgb[2]+min(b[0],b[1])]
+  elif i==(N-1):
+    r = [200,rgb[1]+min(r[0],r[2]),rgb[2]+min(r[0],r[1])]
+    g = [rgb[0]+min(g[1],g[2]),200,rgb[2]+min(g[0],g[1])]
+    b = [rgb[0]+min(b[1],b[2]),rgb[1]+min(b[0],b[2]),200]
   else:
-    result = [[[rgb[0]+min(result[1],result[2])]*2],[[rgb[1]+min(result[0],result[2])]*2],[[rgb[2]+min(result[0],result[1])]*2]]
+    r = [rgb[0]+min(r[1],r[2]),rgb[1]+min(r[0],r[2]),rgb[2]+min(r[0],r[1])]
+    g = [rgb[0]+min(g[1],g[2]),rgb[1]+min(g[0],g[2]),rgb[2]+min(g[0],g[1])]
+    b = [rgb[0]+min(b[1],b[2]),rgb[1]+min(b[0],b[2]),rgb[2]+min(b[0],b[1])]
+print(min(min(r),min(g),min(b)))
--- 1764.py
+++
@@ -1,9 +0,0 @@
-D,B = map(int,input().split())
-li = {}
-for _ in range(D):
-  li[input()] = 0
-
-for _ in range(B):
-  name = input()
-  if li[name]:
-    print(name)
